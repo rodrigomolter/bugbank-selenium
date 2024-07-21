270d0e408f1ffad78bfe810fd7c74b1e75040325
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-21 02:04:00 -0300

    refactor: changing browser setup to match case

--- features/environment.py
+++ features/environment.py
@@ -5,25 +5,27 @@ from ipdb import spost_mortem
 
 
 def before_all(context):
-    browser = context.config.userdata.get('browser').lower()
+    browser = context.config.userdata.get("browser").lower()
 
-    if browser == 'chrome':
-        context.browser = Chrome()
-    elif browser == 'firefox':
-        context.browser = Firefox()
-    elif browser == 'headless chrome':
-        options = ChromeOptions()
-        options.add_argument('--headless=new')
-        options.add_argument('--disable-gpu')
-        context.browser = Chrome(options=options)
-    elif browser == 'headless firefox':
-        options = FirefoxOptions()
-        options.add_argument('-headless')
-        context.browser = Firefox(options=options)
-    else:
-        raise ValueError(f"Unsupported browser: {browser}")
+    match browser:
+        case "chrome":
+            context.browser = Chrome()
+        case "firefox":
+            context.browser = Firefox()
+        case "headless chrome":
+            options = ChromeOptions()
+            options.add_argument("--headless=new")
+            options.add_argument("--disable-gpu")
+            context.browser = Chrome(options=options)
+        case "headless firefox":
+            options = FirefoxOptions()
+            options.add_argument("-headless")
+            context.browser = Firefox(options=options)
+        case _:
+            raise ValueError(f"Unsupported browser: {browser}")
+
     
-    context.browser.implicitly_wait(5)
+    # context.browser.implicitly_wait(5)
     # context.base_url = context.config.userdata.get('base_url')
 
 def before_scenario(context, scenario):
@@ -41,5 +43,5 @@ def after_all(context):
     context.browser.quit()
 
 def after_step(context, step):
-    if context.config.userdata.getbool("debug") and step.status == 'failed':
+    if context.config.userdata.getbool("debug") and step.status == "failed":
         spost_mortem(step.exc_traceback)
