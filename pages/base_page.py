832cece4c9c01ec4d16a58a7bc17b453fd8573eb
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-27 11:39:27 -0300

    refactor: getting base URL from behave.ini

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,15 +2,15 @@ from abc import ABC
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from utils.utils import Utils
 
 class BasePage(ABC):
-  def __init__(self, webdriver: webdriver, base_url: str = 'https://bugbank.netlify.app') -> None:
+  def __init__(self, webdriver: webdriver) -> None:
     self.webdriver: webdriver = webdriver
-    self.base_url = base_url
+    self.base_url = Utils.get_app_url()
 
   def find_element(self, locator: tuple[By, str]) -> WebElement:
     return self.webdriver.find_element(*locator)
@@ -38,6 +38,7 @@ class BasePage(ABC):
         return False
     
   def delete_browser_data(self) -> None:
+    self.open()
     self.webdriver.execute_script("window.localStorage.clear();")
     self.webdriver.delete_all_cookies()
     self.webdriver.refresh()
